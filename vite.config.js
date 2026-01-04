4368f31d97afebace81e463f3d7fc705cd3c1835
Kokiaibot <mimeiakainman@gmail.com>
2026-01-04 21:07:14 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html')
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
   server: {
     host: '0.0.0.0',
     port: 3000
